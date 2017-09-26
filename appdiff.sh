71b9f777d9fd46eedbe56778d89b042dcaf8450d
IS <isramko@gmail.com>
2017-09-26 15:00:28 +0200

    appdiff script - testing4

--- appdiff.sh
+++ appdiff.sh
@@ -17,12 +17,21 @@ BRANCH="master"
 ## working dir
 cd /root/app
 
-##GITDIFF=`git diff --name-only --ignore-space-change gh/$BRANCH`
-#GITDIFF=`git diff --name-only --ignore-space-change`
-GITDIFF=`git log master..origin/master -1`
-len=${#GITDIFF}
+###GITDIFF=`git diff --name-only --ignore-space-change gh/$BRANCH`
+##GITDIFF=`git diff --name-only --ignore-space-change`
+#GITDIFF=`git log master..origin/master -1`
+#len=${#GITDIFF}
 
+#if [ $len -lt 3 ]
+
+#VER_LOCAL=`git log -1 --oneline origin/master`
+#VER_REMOTE=`git log -1 --oneline origin/master...HEAD`
+VER_REMOTE=`git ls-remote https://github.com/isra67/indoorjs.git | grep master | cut -f 1`
+GITDIFF=`git cherry -v | grep $VER_REMOTE`
+len=${#GITDIFF}
 if [ $len -lt 3 ]
+
+#if [ "$VER_LOCAL" == "$VER_REMOTE" ]
 then
 
     echo "Nothing to do"
